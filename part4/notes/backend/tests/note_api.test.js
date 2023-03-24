3f5fadf85e12090de12a3cd253717b74571f8108
Baptiste Dromer <baptiste.dromer@gmail.com>
2023-03-24 16:19:52 +0100

    Fix 4.x bug in app.js

---
+++ part4/notes/backend/tests/note_api.test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+
+const api = supertest(app); //returns a superagent object
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200) // response code should be 200
+    .expect('Content-Type', /application\/json/); // response header should match regexp
+});
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes');
+
+  expect(response.body).toHaveLength(2);
+});
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes/');
+
+  expect(response.body[0].content).toBe('HTML is easy');
+});
+
+afterAll(async () => await mongoose.connection.close());
