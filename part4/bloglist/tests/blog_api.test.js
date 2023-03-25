8286d733601dcc4ffd89041fe73fc320297f2927
Baptiste Dromer <baptiste.dromer@gmail.com>
2023-03-25 23:01:04 +0100

    4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -40,4 +40,29 @@ test('blogs have an unique identifier property named id', async () => {
   response.body.map((blogs) => expect(blogs.id).toBeDefined());
 });
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    author: 'Alice',
+    title: 'I like flowers',
+    url: 'http://aliceinwonderland.com',
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  // check number of blogs is the system is increased by one
+  const blogAtEnd = await helper.blogsInDb();
+  expect(blogAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  // check that the title of blog is properly saved in db
+  const titles = blogAtEnd.map((blog) => blog.title);
+  expect(titles).toContain(helper.initialBlogs[0].title);
+});
+
+// 4.12
+// test('a blog without title cannot be added', async () => {});
+
 afterAll(async () => await mongoose.connection.close());
