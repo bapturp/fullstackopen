8f873dcbe5a2b46abfa86d1216fed167bea2d894
Baptiste Dromer <baptiste.dromer@gmail.com>
2024-05-11 12:15:45 +0200

    exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,61 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const Statistics = ({good, bad, neutral}) => {
+  if (good === 0 && bad === 0 && neutral === 0) {
+    return (
+      <div>No feeback yet</div>
+    )
+  }
+  const all = good + bad + neutral
+  const average = (good + bad*-1) / all
+  const positive = good * 100 / all
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good}></StatisticLine>
+        <StatisticLine text="neutral" value={neutral}></StatisticLine>
+        <StatisticLine text="bad" value={bad}></StatisticLine>
+        <StatisticLine text="all" value={all}></StatisticLine>
+        <StatisticLine text="average" value={average}></StatisticLine>
+        <StatisticLine text="positive" value={positive}></StatisticLine>
+      </tbody>
+    </table>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <h2>give feedback</h2>
+      <Button handleClick={handleGood} text="Good"></Button>
+      <Button handleClick={handleNeutral} text="Neutral"></Button>
+      <Button handleClick={handleBad} text="Bad"></Button>
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
